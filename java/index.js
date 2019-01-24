4d94e89c967a48ff1a370f08235406fff5b5b6f8
Falkrik <46990247+Falkrik@users.noreply.github.com>
2019-01-24 09:35:59 +0100

    Nextgen comet

---
+++ java/index.js
@@ -0,0 +1,27 @@
+/*Javascript-Dokument*/
+
+
+function loadPage(url, pix) {
+
+	if(url == 'home.html')
+	{
+		window.scrollTo(0,0);
+	} else {
+	window.scrollTo(0, 450);
+	}
+
+	var tid = new Date().getTime();
+	var end = tid;
+
+
+	while(end < tid + 100) {
+		end =  new Date().getTime();
+	}
+
+	var frame = document.getElementById("BigFrame");
+	frame.src = url;
+
+
+	frame.style.height = pix + 'px';
+
+}
